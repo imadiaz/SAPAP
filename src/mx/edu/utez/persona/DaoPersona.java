36412b62bd2a2922d0895d79ac69f54714fe3d2f
valverdemario <50206886+valverdemario@users.noreply.github.com>
2019-11-19 18:49:13 -0600

    [ACTUALIZAR]
    LOGIN
    ROLES
    RECUPERAR CONTRASEÑA

--- src/mx/edu/utez/persona/DaoPersona.java
+++ src/mx/edu/utez/persona/DaoPersona.java
@@ -1,5 +1,6 @@
 package mx.edu.utez.persona;
 
+import com.mysql.cj.x.protobuf.MysqlxDatatypes;
 import mx.edu.utez.conexion.MySQLConexion;
 import mx.edu.utez.rol.BeanRol;
 import org.apache.struts2.components.Bean;
@@ -16,19 +17,21 @@ public class DaoPersona {
     private PreparedStatement pstm;
     private ResultSet rs;
 
-    final private String SQL_CONSULTAR_PERSONA ="call login(?,?)";
+    final private String SQL_CONSULTAR_PERSONA = "call login(?,?)";
+    final private String SQL_GENERAR_CODIGO = "call generarCodigo(?,?)";
+    final private String SQL_Consultar_Correo = "Select * from Persona where correoInstitucional=?;";
 
 
-    public BeanPersona consultarPersonas(String correo, String contra){
-       BeanPersona bean=null ;
-        try{
+    public BeanPersona consultarPersonas(String correo, String contra) {
+        BeanPersona bean = null;
+        try {
             conexion = MySQLConexion.getConnection();
             pstm = conexion.prepareStatement(SQL_CONSULTAR_PERSONA);
-            pstm.setString(1,correo);
-            pstm.setString(2,contra);
+            pstm.setString(1, correo);
+            pstm.setString(2, contra);
             rs = pstm.executeQuery();
-            while (rs.next()){
-               bean = new BeanPersona();
+            while (rs.next()) {
+                bean = new BeanPersona();
                 bean.setIdPersona(rs.getInt("id"));
                 bean.setNombre(rs.getString("nombre"));
                 bean.setPrimerApellido(rs.getString("nombre"));
@@ -40,28 +43,98 @@ public class DaoPersona {
                 bean.setContrasenia(rs.getString("contrasenia"));
 
             }
-            if (bean==null){
+            if (bean == null) {
                 System.out.println("es nulo");
-            }else{
+            } else {
                 System.out.println(bean.getNombre());
             }
             rs.close();
             pstm.close();
             conexion.close();
-        }catch (Exception e){
-            System.out.println("Error en el metodo consultar personas: "+e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Error en el metodo consultar personas: " + e.getMessage());
             System.out.println(e.getCause());
-        }finally {
-            try{
+        } finally {
+            try {
                 rs.close();
                 pstm.close();
                 conexion.close();
-            }catch(Exception e){
+            } catch (Exception e) {
 
             }
         }
         return bean;
     }
 
+    public BeanPersona consultarCorreo(String correo){
+        BeanPersona bean = null;
+        try{
+            conexion = MySQLConexion.getConnection();
+            pstm = conexion.prepareStatement(SQL_Consultar_Correo);
+            pstm.setString(1,correo);
+            rs = pstm.executeQuery();
+            while (rs.next()) {
+                bean = new BeanPersona();
+                bean.setIdPersona(rs.getInt("id"));
+                bean.setNombre(rs.getString("nombre"));
+                bean.setPrimerApellido(rs.getString("nombre"));
+                bean.setSegundoApellido(rs.getString("nombre"));
+                bean.setFechaDeNacimiento(rs.getString("fechaNacimiento"));
+                bean.setCorreoInstitucional(rs.getString("correoInstitucional"));
+                bean.setCorreoPersonal(rs.getString("correoInstitucional"));
+                bean.setMatricula(rs.getString("matricula"));
+                bean.setContrasenia(rs.getString("contrasenia"));
+                bean.setCodigo(rs.getString("codigo"));
+
+            }
+            if (bean == null) {
+                System.out.println("es nulo");
+            } else {
+                System.out.println(bean.getNombre());
+            }
+            rs.close();
+            pstm.close();
+            conexion.close();
+        } catch (Exception e) {
+            System.out.println("Error en el metodo consultar correo: " + e.getMessage());
+            System.out.println(e.getCause());
+        } finally {
+            try {
+                rs.close();
+                pstm.close();
+                conexion.close();
+            } catch (Exception e) {
+
+            }
+        }
+        return bean;
+    }
+
+
+    public void codigo(String correo, String codigo) {
+        boolean res = false;
+        try {
+            conexion = MySQLConexion.getConnection();
+            pstm = conexion.prepareStatement(SQL_GENERAR_CODIGO);
+            pstm.setString(1, correo);
+            pstm.setString(2, codigo);
+            rs = pstm.executeQuery();
+            res=true;
+            rs.close();
+            pstm.close();
+            conexion.close();
+        } catch (Exception e) {
+            System.out.println("Error en el metodo consultar personas: " + e.getMessage());
+            System.out.println(e.getCause());
+        } finally {
+            try {
+                rs.close();
+                pstm.close();
+                conexion.close();
+            } catch (Exception e) {
+
+            }
+        }
+    }
 
 }
